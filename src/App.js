67031c2219b3b4ad0e4b926dc8b17be43af169de
mashadservice <safa.mobi.380@gmail.com>
2022-09-12 13:21:28 +0430

    adding first setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import ExpenceBox from "./components/Expences/ExpenceBox";
+import AddExpenceBox from "./components/AddExpences/AddExpenceBox";
+import Chart from "./components/Chart/Chart";
+
+const initialExpenceList = [
+  {
+    Id: 1,
+    Title: "خرید گوشی از فروشنده در خیابان احمد آباد",
+    Price: 19000,
+    Date: "۱۴۰۱/۰۷/۱۵",
+    Time: "۲۰ : ۱۷",
+    Statuse: "پرداخت شده",
+  },
+  {
+    Id: 2,
+    Title: "خرید مانیتور از فروشگاه اینترنتی دیجی کالا",
+    Price: 15000,
+    Date: "۱۴۰۰/۰۳/۱۵",
+    Time: "۲۰ : ۱۷",
+    Statuse: "در انتظار پرداخت",
+  },
+];
 
 function App() {
+  const [expenceList, addingNewExpence] = useState(initialExpenceList);
+
+  const addingToExpenseList = (newExpenceData) => {
+    addingNewExpence((prevExpenceList) => {
+      return [newExpenceData, ...prevExpenceList];
+    });
+  };
+
+  const deletingItem = (deleteKey) => {
+    addingNewExpence((prevGoals) => {
+      const expenceList = prevGoals.filter((item) => item.Id !== deleteKey);
+      return expenceList;
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <AddExpenceBox onAddingNewExpence={addingToExpenseList} />
+      <Chart items={expenceList} />
+      <ExpenceBox items={expenceList} onDelete={deletingItem} />
     </div>
   );
 }
